c035fddd71b25148f1b61bbaeb9cddc3cf30c0f8
haeun <103066456+haeun9634@users.noreply.github.com>
2025-01-14 16:09:43 +0900

    Delete chating/src/main directory

--- chating/src/main/java/com/example/chating/domain/chat/Repository/ChatRoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.domain.chat.Repository;
-
-import com.example.domain.chat.ChatRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {}
-
--- chating/src/main/java/com/example/chating/global/config/SwaggerConfig.java
+++
@@ -1,2 +0,0 @@
-package com.example.chating.global.config;public class SwaggerConfig {
-}
